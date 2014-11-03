72b314be9e49f570a364277e947f3b610a45138d
Mark Gibson <mgibson@adaptavist.com>
2014-11-03 11:37:25 +0000

    display url in embedded page

--- container/tracker.js
+++ container/tracker.js
@@ -1,22 +1,32 @@
+var App = {};
+
+App.switchOrigin = function() {
+    var origin = "http://jollytoad.github.io/iframe-url-tracking-prototype/embedded/index.html";
+
+    App.iframe1.src = origin;
+    App.iframe2.src = origin;
+};
+
+App.setUrlFromIframe = function(iframe, url) {
+    url.innerText = iframe.contentWindow.location;
+};
+
 window.onload = function() {
 
-    var iframe1 = document.getElementById("iframe1");
-    var url1 = document.getElementById("url1");
+    App.iframe1 = document.getElementById("iframe1");
+    App.url1 = document.getElementById("url1");
 
-    var iframe2 = document.getElementById("iframe2");
-    var url2 = document.getElementById("url2");
+    App.iframe2 = document.getElementById("iframe2");
+    App.url2 = document.getElementById("url2");
 
-    function setUrlFromIframe(iframe, url) {
-        url.innerText = iframe.contentWindow.location;
-    }
-
+    // Fetch iframe1 url using interval
     setInterval(function() {
-        setUrlFromIframe(iframe1, url1);
-//        setUrlFromIframe(iframe2, url2);
+        App.setUrlFromIframe(App.iframe1, App.url1);
     }, 1000);
 
-    setUrlFromIframe(iframe2, url2);
-    iframe2.onload = function() {
-        setUrlFromIframe(iframe2, url2);
+    // Fetch iframe2 url using onload event
+    App.setUrlFromIframe(App.iframe2, App.url2);
+    App.iframe2.onload = function() {
+        App.setUrlFromIframe(App.iframe2, App.url2);
     };
 };
---
+++ embedded/url.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+    document.getElementById("url").innerText = window.location;
+};
