35547aa9e8aec6b844e426b948aab74b4d2e02fb
Mark Gibson <mgibson@adaptavist.com>
2014-11-03 12:00:50 +0000

    send url to parent via postMessage

--- container/tracker.js
+++ container/tracker.js
@@ -1,7 +1,7 @@
 var App = {};
 
 App.switchOrigin = function() {
-    var origin = "http://jollytoad.github.io/iframe-url-tracking-prototype/embedded/index.html";
+    var origin = "https://jollytoad.github.io/iframe-url-tracking-prototype/embedded/index.html";
 
     App.iframe1.src = origin;
     App.iframe2.src = origin;
@@ -20,9 +20,19 @@ window.onload = function() {
     App.url2 = document.getElementById("url2");
 
     // Fetch iframe1 url using interval
-    setInterval(function() {
-        App.setUrlFromIframe(App.iframe1, App.url1);
-    }, 1000);
+//    setInterval(function() {
+//        App.setUrlFromIframe(App.iframe1, App.url1);
+//    }, 1000);
+
+    // Fetch iframe1 url from a message event
+    window.addEventListener("message", function(event) {
+        console.log("message from ", event.origin, event.source, " -> ", event.data);
+
+        if (event.source === App.iframe1.contentWindow && event.data.url) {
+            console.log("set url from message");
+            App.url1.innerText = event.data.url;
+        }
+    });
 
     // Fetch iframe2 url using onload event
     App.setUrlFromIframe(App.iframe2, App.url2);
--- embedded/url.js
+++ embedded/url.js
@@ -1,3 +1,9 @@
 window.onload = function() {
     document.getElementById("url").innerText = window.location;
+
+    console.log(parent);
+
+    if (parent) {
+        parent.postMessage({ url: window.location.toString() }, "*");
+    }
 };
